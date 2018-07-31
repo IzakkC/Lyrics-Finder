9f5741792d1dc3dcea1e9bdcf1b73dbff7ca97d9
Izakk Carrillo <izakkcarrillo@skills-macbook-pro-15.local>
2018-07-31 10:27:19 -0400

    Initial Commit

---
+++ Lyrics Finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lyrics Finder
+//
+//  Created by Izakk Carrillo on 7/31/18.
+//  Copyright © 2018 Izakk Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
